c61044d1fa19dbc091951aed33fb7ec195d8a51d
Rohit_Joshi <rohitjoshi632001@gmail.com>
2020-10-26 21:20:50 +0530

    Insertion sort

---
+++ Sorting_algo/Insertion_Sort.py
@@ -0,0 +1,15 @@
+def insertionSort(arr):
+   for i in range(1,len(arr)):
+
+     currentvalue = arr[i]
+     position = i
+
+     while position>0 and arr[position-1]>currentvalue:
+         arr[position]=arr[position-1]
+         position = position-1
+
+     arr[position]=currentvalue
+
+arr = list(map(int,input().split(" ")))
+insertionSort(arr)
+print(arr)
